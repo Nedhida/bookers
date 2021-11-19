67ba4f06ffbb2139dbc784547f89707892da3709
EC2 Default User <ec2-user@ip-172-31-20-19.ec2.internal>
2021-11-19 06:04:18 +0000

    [Start]ルーティングの記述変更

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  # get root (/) 時にここに遷移する設定
+  root to: 'homes#top'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+end
